f01dc675fe12d2579d44481c28c0b0fb256da05d
fatimabolotbek <zhumalieve91@gmail.com>
2024-06-26 02:34:10 +0000

    push

---
+++ loops.py
@@ -0,0 +1,11 @@
+## for loop 
+# for letter in ["New York", "Chicago", "phonix"]:
+#     if letter == "Chicago":
+#      print("Welcome to", letter)
+     
+## While loop 
+  
+x = 0
+while x <= 6:
+    x += 3
+    print(x)
